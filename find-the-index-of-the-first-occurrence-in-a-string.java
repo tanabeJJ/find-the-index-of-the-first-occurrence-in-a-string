6bf96af983bece1b3bfd892465091483fc72ed5d
tanabeJJ <tanabe@japannext.net>
2023-10-13 19:15:31 +0900

    f

---
+++ find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static void main(String[] args) {
+      Solution ins = new Solution();
+      System.out.println(ins.strStr("sadbutsad","sad") == 0);
+      System.out.println(ins.strStr("leetcode","leeto") == -1);
+      System.out.println(ins.strStr("leetcode","tco") == 3);
+      System.out.println("すべてTrueならコンプリート！");
+    }
+
+    public int strStr(String haystack, String needle)  {
+        // ここにコードを書いてください
+        return 5;
+    }
+}
---
+++ find-the-index-of-the-first-occurrence-in-a-string.py
@@ -0,0 +1,13 @@
+from typing import *
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        # ここにコードを書いてください
+        return 0
+
+
+print("スタート")
+assert Solution().strStr("sadbutsad","sad") == 0
+assert Solution().strStr("leetcode","leeto") == -1
+assert Solution().strStr("leetcode","tco") == 3
+print("コンプリート!本番テストへ")
